836c2501ed3230ea30fba1073deba8580a761ff4
datdev <dnguyentien145@gmail.com>
2024-11-13 08:48:22 +0700

    update code

--- resources/views/frontend/layouts/partials/head.blade.php
+++ resources/views/frontend/layouts/partials/head.blade.php
@@ -1,12 +1,8 @@
 <meta charset="UTF-8" />
 <title>{{ $config->title_seo ?? $config->name }}</title>
-<meta http-equiv="Cache-Control" content="no-cache" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta http-equiv="Expires" content="-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="keywords" content="{{ $config->keywords_seo }}" />
 <meta name="description" content="{{ $config->description_seo }}" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-
 
 <link rel="icon" href="{{ showImage($config->icon) }}" type="image/png" />
 
